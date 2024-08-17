487abd2bec112e38df83af2e690028c2456680a2
L4852 <122051988+L4852@users.noreply.github.com>
2024-08-16 18:14:51 -0700

    Initial commit

---
+++ src/main/java/com/vanillablocks/VanillaBlocksClient.java
@@ -0,0 +1,10 @@
+package com.vanillablocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanillaBlocksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/vanillablocks/VanillaBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package com.vanillablocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
